01cf9f2e9c7849af100c2c0520e7e4e97c183b63
Bill Trikalinos <billtrik@gmail.com>
2018-07-21 21:52:24 +0200

    Create base layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import Button from 'antd/lib/button';
-import './App.css';
+import React from 'react';
+import { Layout } from 'antd';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    );
-  }
-}
+import DroneList from './pages/dronesList';
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+  return (
+    <Layout>
+      <Header>
+        <h1> Welcome to the Drone Manager</h1>
+      </Header>
+      <Layout>
+        <Sider />
+        <Content>
+          <DroneList />
+        </Content>
+        <Sider />
+      </Layout>
+    </Layout>
+  );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux'
 import configureStore from './store';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
 
 // HYDRATE MANUALLY THE STORE FOR DEVELOPMENT OF UI
 // TODO: remove after websockets are setup

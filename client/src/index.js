af3fa81a95f5375517f53e6a90e0d0197e7b7c7c
Bill Trikalinos <billtrik@gmail.com>
2018-07-21 21:49:55 +0200

    Hydrate store with sample Drone data

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,13 @@ import configureStore from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// HYDRATE MANUALLY THE STORE FOR DEVELOPMENT OF UI
+// TODO: remove after websockets are setup
+import { hydrateDroneData } from './store/drones/actions';
+import payload from './payload.json';
 
 const store = configureStore();
+store.dispatch( new hydrateDroneData(payload) );
 
 ReactDOM.render(
   <Provider store={store}>

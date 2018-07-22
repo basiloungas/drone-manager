b5e7ce5866501339bbf688db094f226caca490f4
Bill Trikalinos <billtrik@gmail.com>
2018-07-22 19:05:20 +0200

    Use SessionMnager to retrieve data for the app

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+
 import configureStore from './store';
+import SessionManager from './modules/session-manager';
+import generateEventsList from './modules/session-manager/events-list';
+
 import App from './App';
-import './index.css';
-
-
-// HYDRATE MANUALLY THE STORE FOR DEVELOPMENT OF UI
-// TODO: remove after websockets are setup
-import { hydrateDroneData } from './store/drones/actions';
-import payload from './payload.json';
 
 const store = configureStore();
-store.dispatch( new hydrateDroneData(payload) );
+
+new SessionManager({
+  url: 'http://localhost:4001', //TODO: should be process.env.URL
+  eventsList: generateEventsList(store),
+});
 
 ReactDOM.render(
   <Provider store={store}>

35a89aa8b8e277709a57a6326336f9122f8cbd6e
Bill Trikalinos <billtrik@gmail.com>
2018-07-22 11:53:13 +0200

    fixup! Add Ant dependency

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,7 +1,5 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
-console.log('asdasdasd');
-
 module.exports = function override(config, env) {
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
   return config;
